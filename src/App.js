5b62adf7c1fb9965160fe3a1e77800b826fba4c5
Lucas Gonçalves <lgg9083@gmail.com>
2024-06-26 22:17:23 -0300

    feat: adiciona correção estrutural

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
-import NavBar from './components/layouts/Navbar'
-import Footer from './components/layouts/Footer'
+import NavBar from "./components/layouts/Navbar";
+import Footer from "./components/layouts/Footer";
+import Container from "./components/layouts/Container";
 function App() {
   return (
     <Router>
-      <NavBar/>
-      <Routes>
-        <Route path="/" Component={Home}/>
-        <Route path="/Login" Component={Login}/>
-        <Route path="/Register" Component={Register}/>
-      </Routes>
-      <Footer/>
+      <NavBar />
+      <Container>
+        <Routes>
+          <Route path="/" Component={Home} />
+          <Route path="/Login" Component={Login} />
+          <Route path="/Register" Component={Register} />
+        </Routes>
+      </Container>
+      <Footer />
     </Router>
   );
 }
