c006953d4ca72571654f83fef666181136187bca
Lucas Gonçalves <lgg9083@gmail.com>
2024-06-26 22:09:49 -0300

    feat: adiciona navbar e footer ao projeto

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Auth/Login";
+import Register from "./pages/Auth/Register";
+import NavBar from './components/layouts/Navbar'
+import Footer from './components/layouts/Footer'
 function App() {
   return (
-    <div className="App">
-    <h1>Get A Pet</h1>
-    </div>
+    <Router>
+      <NavBar/>
+      <Routes>
+        <Route path="/" Component={Home}/>
+        <Route path="/Login" Component={Login}/>
+        <Route path="/Register" Component={Register}/>
+      </Routes>
+      <Footer/>
+    </Router>
   );
 }
 
