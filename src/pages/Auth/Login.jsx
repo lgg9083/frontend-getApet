3db5a1f09088f87a7433c9d456b232ad65259204
Lucas Gonçalves <lgg9083@gmail.com>
2024-06-28 23:08:26 -0300

    feat: adiciona pagina para login na aplicação

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,10 +1,39 @@
+import { useState, useContext } from "react";
+import Input from "../../components/form/Input";
+import styles from "../../components/form/Form.module.css";
+import { context } from "../../context/UserContext";
+import { Link } from "react-router-dom";
+
 function Login() {
-    return (
-      <section>
-        <h1>Login</h1>
-      </section>
-    );
+  function handleChange(e) {
+    return "";
   }
-  
-  export default Login;
-  
+  return (
+    <section className={styles.form_container}>
+      <h1>Login</h1>
+      <form>
+        <Input
+          text="E-mail"
+          type="email"
+          name="email"
+          placeholder="Digite seu email"
+          handleOnChange={handleChange}
+        />
+      </form>
+      <form>
+        <Input
+          text="Senha"
+          type="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+        />
+
+        <Input type="submit" value="Entra" />
+      </form>
+      <p>Não tem conta? <Link to='/register'>Clique aqui!</Link></p>
+    </section>
+  );
+}
+
+export default Login;
