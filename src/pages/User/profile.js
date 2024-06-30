9448c5b6375d3deabbcc4273ad5da9115e15f0c4
Lucas Gonçalves <lgg9083@gmail.com>
2024-06-30 19:21:08 -0300

    feat: adiciona interface para edição de usuario

--- src/pages/User/profile.js
+++ src/pages/User/profile.js
@@ -1,5 +1,72 @@
+import styles from "./profile.module.css";
+import formStyles from "../../components/form/Form.module.css";
+import Input from "../../components/form/Input";
+import { useState, useEffect, useContext } from "react";
+
 function Profile() {
-  return <h1>Hello</h1>
+  const [user, setUser] = useState({});
+
+  function onFileChange() {
+    return;
+  }
+  function handleChange() {
+    return;
+  }
+  return (
+    <section>
+      <div className={styles.profile_container}>
+        <h1>Perfil</h1>
+        <p>Previw de imagem </p>
+      </div>
+      <form className={formStyles.form_container}>
+        <Input
+          text="Imagem"
+          type="file"
+          name="image"
+          handleOnChange={onFileChange}
+        />
+        <Input
+          text="Email"
+          type="text"
+          name="email"
+          placeholder="Digite o seu email"
+          handleOnChange={handleChange}
+          value={user.email || ""}
+        />
+        <Input
+          text="Nome"
+          type="text"
+          name="name"
+          placeholder="Digite o seu nome"
+          handleOnChange={handleChange}
+          value={user.name || ""}
+        />
+        <Input
+          text="Telefone"
+          type="text"
+          name="phone"
+          placeholder="Digite o seu telefone"
+          handleOnChange={handleChange}
+          value={user.telefone || ""}
+        />
+        <Input
+          text="senha"
+          type="password"
+          name="password"
+          placeholder="Digite a sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input
+          text="senha"
+          type="password"
+          name="confirmPassword"
+          placeholder="confirme a sua senha"
+          handleOnChange={handleChange}
+        />
+        <Input type="submit" value="Editar" />
+      </form>
+    </section>
+  );
 }
 
 export default Profile;
