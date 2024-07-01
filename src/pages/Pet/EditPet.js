ddea8d630134f18b20f303d9038b4a62a13cc5ce
Lucas Gonçalves <lgg9083@gmail.com>
2024-07-01 19:33:22 -0300

    feat: adiciona metodo para recuperar os dados do pet para edição

--- src/pages/Pet/EditPet.js
+++ src/pages/Pet/EditPet.js
@@ -1,17 +1,36 @@
 import api from "../../utils/api";
 import { useState, useEffect } from "react";
 import styles from "./AddPet.module.css";
-
+import { useParams } from "react-router-dom";
 import PetForm from "../../components/form/PetForm";
 import useFlashMessages from "../../hooks/UseFlashMessage";
 function EditPet() {
   const [pet, setPet] = useState({});
+  const [token] = useState(localStorage.getItem("token") || "");
+  const { id } = useParams();
+  const { setFlashMessages } = useFlashMessages();
+
+  useEffect(() => {
+    api
+      .get(`/pets/${id}`, {
+        headers: {
+          Authorization: `Bearer ${JSON.parse(token)}`,
+        },
+      })
+      .then((response) => {
+        setPet(response.data.pet);
+      });
+  }, [token, id]);
+  async function updatedPet(pet) {}
   return (
     <section>
       <div className={styles.adpoter_header00000}>
-        <h1>Editando o Pet : {pet.nome}</h1>
-        <p>Depois da edição os dado serão atualizados no sistemas</p>
+        <h1>Editando o Pet : {pet.name}</h1>
+        <p>Depois da edição os dado serão atualizados no sistema</p>
       </div>
+      {pet.name && (
+        <PetForm handleSubmit={updatedPet} btnText="Atualizar" petData={pet} />
+      )}
     </section>
   );
 }
