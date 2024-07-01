d98486a604ab02b5630ce3b95cad12008f6d508f
Lucas Gonçalves <lgg9083@gmail.com>
2024-07-01 20:48:53 -0300

    feat: adiciona logica para buscar detalhes individuais de cada pet

--- src/pages/Pet/PetDetails.js
+++ src/pages/Pet/PetDetails.js
@@ -4,8 +4,17 @@ import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import useFlashMessages from "../../hooks/UseFlashMessage";
 function PetDetails() {
-    
-  return <div>PetDetails</div>;
+  const [pet, setPet] = useState({});
+  const { id } = useParams();
+  const { setFlashMessages } = useFlashMessages();
+  const [token] = useState(localStorage.getItem("token") || "");
+
+  useEffect(() => {
+    api.get(`/pets/${id}`).then((response) => {
+      setPet(response.data.pet);
+    });
+  }, [id]);
+  return <h1>{pet.name}</h1>;
 }
 
 export default PetDetails;
