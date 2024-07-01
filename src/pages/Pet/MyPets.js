1329790e82d3872d821cb7d98bf9bda39888ce91
Lucas Gonçalves <lgg9083@gmail.com>
2024-06-30 23:37:41 -0300

    feat: adiciona pagina para listar os pets, adiciona a estilização da mesma

--- src/pages/Pet/MyPets.js
+++ src/pages/Pet/MyPets.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import api from "../../utils/api";
 import RoundImage from "../../components/layouts/RoundedImage";
 import useFlashMessages from "../../hooks/UseFlashMessage";
+import styles from "./Dashboard.module.css";
 function MyPets() {
   const [pets, setPets] = useState([]);
   const [token] = useState(localStorage.getItem("token") || "");
@@ -16,20 +17,44 @@ function MyPets() {
         },
       })
       .then((response) => {
-        console.log(token
-
-        )
+        console.log(token);
         setPets(response.data.pets);
       });
   }, [token]);
   return (
     <section>
-      <div>
+      <div className={styles.petlist_header}>
         <h1>MyPets</h1>
         <Link to="/pets/add">Cadastrar Pet</Link>
       </div>
-      <div>
-        { pets.length > 0 && <p>Meus Pets cadastrados</p>}
+      <div className={styles.petlist_container}>
+        {pets.length > 0 &&
+          pets.map((pet) => (
+            <div className={styles.petlist_row}>
+              <RoundImage
+                src={`${process.env.REACT_APP_API}/images/pets/${pet.images[0]}`}
+                alt={pet.name}
+                width="px75"
+              />
+              <span className="bold">{pet.name}</span>
+              <div className={styles.actions}>
+                {pet.available ? (
+                  <>
+                    {console.log(pet)}
+                    {pet.adpoter && (
+                      <button className={styles.conclude_btn}>
+                        concluir adução
+                      </button>
+                    )}
+                    <Link to={`/pets/edit/${pet._id}`}>Editar</Link>
+                    <button> Excluir</button>
+                  </>
+                ) : (
+                  <p>Pet ja Adotado</p>
+                )}{" "}
+              </div>
+            </div>
+          ))}
         {pets.length === 0 && <p> Não há Pets cadastrados</p>}
       </div>
     </section>
