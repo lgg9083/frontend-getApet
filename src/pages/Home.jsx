d47fdf7439343faab62a7ee3e036be26d9c791c4
Lucas Gonçalves <lgg9083@gmail.com>
2024-07-01 19:56:03 -0300

    feat: adiciona logica para recuperar todos os pets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,18 @@
+import api from "../utils/api";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import styles from "./Home.module.css";
 function Home() {
+  const [pet, setPets] = useState({});
+  useEffect(() => {
+    api.get("/pets").then((response) => {
+      setPets(response.data.pets);
+    });
+  }, []);
   return (
     <section>
-      <h1>Hello</h1>
+      <h1>Home</h1>
     </section>
   );
 }
