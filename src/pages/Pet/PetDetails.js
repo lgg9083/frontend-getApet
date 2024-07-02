676453db9cf731c8162412ae578cc069c9cbc3ec
Lucas Gonçalves <lgg9083@gmail.com>
2024-07-01 21:05:59 -0300

    feat: adiciona loop para reenderizar as imagens de cada pet

--- src/pages/Pet/PetDetails.js
+++ src/pages/Pet/PetDetails.js
@@ -3,6 +3,7 @@ import api from "../../utils/api";
 import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import useFlashMessages from "../../hooks/UseFlashMessage";
+import { all } from "axios";
 function PetDetails() {
   const [pet, setPet] = useState({});
   const { id } = useParams();
@@ -14,7 +15,42 @@ function PetDetails() {
       setPet(response.data.pet);
     });
   }, [id]);
-  return <h1>{pet.name}</h1>;
+  return (
+    <>
+      {pet.name && (
+        <section>
+          <div>
+            <h1>Conhecendo o Pet: {pet.name}</h1>
+            <p> Se tiver interesse, marque uma visita para conhecê-lo</p>
+          </div>
+
+          <div>
+            {pet.images.map((image, index) => (
+              <img
+                src={`${process.env.REACT_APP_API}/images/pets/${image}`}
+                all={pet.name}
+                key={index}
+              />
+            ))}
+          </div>
+          <p>
+            <span className="bold">Idade:</span> {pet.age} anos
+          </p>
+          <p>
+            <span className="bold">Peso:</span> {pet.weight}kg
+          </p>
+          {token ? (
+            <button> Solicitar uma visita</button>
+          ) : (
+            <p>
+              Você precisa <Link to="/register">criar uma conta</Link> para
+              solicitar uma visita
+            </p>
+          )}
+        </section>
+      )}
+    </>
+  );
 }
 
 export default PetDetails;
